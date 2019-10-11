0e53ec93f7d5d1284b3e82bb99ea56759bbb45cd
Travis Swicegood <travis@domain51.com>
2019-10-11 12:43:21 -0500

    Refactor to allow overriding the payload in all of the resolvers

--- django_ariadne_jwt/resolvers.py
+++ django_ariadne_jwt/resolvers.py
@@ -26,49 +26,55 @@ auth_token_verification_definition = gql(
 )
 
 
-class TokenAuthResolver:
-    def get_payload(self, user):
-        return {"token": load_backend().create(user) if user else None}
+class BaseTokenResolver:
+    def get_token(self):
+        raise NotImplementedError()
+
+    def get_payload(self):
+        return {"token": self.get_token()}
+
+
+class TokenAuthResolver(BaseTokenResolver):
+    def get_token(self):
+        return load_backend().create(self.user) if self.user else None
 
     def __call__(self, parent, info, **credentials):
-        user = authenticate(info.context, **credentials)
-        return self.get_payload(user)
+        self.user = authenticate(info.context, **credentials)
+        return self.get_payload()
 
 
 # TODO Add DeprecationWarning?
 resolve_token_auth = TokenAuthResolver()
 
 
-class RefreshTokenResolver:
+class RefreshTokenResolver(BaseTokenResolver):
+    def get_token(self):
+        try:
+            return load_backend().refresh(self.token)
+        except (InvalidTokenError, MaximumTokenLifeReachedError):
+            pass
+
     def __call__(self, parent, info, token):
         """Resolves the resfresh token mutaiton"""
-
-        try:
-            token = load_backend().refresh(token)
-
-        except (InvalidTokenError, MaximumTokenLifeReachedError):
-            token = None
-
-        return {"token": token}
+        self.token = token
+        return self.get_payload()
 
 
 resolve_refresh_token = RefreshTokenResolver()
 
 
 class VerifyTokenResolver:
+    def get_payload(self):
+        try:
+            decoded = load_backend().decode(self.token)
+            return {"valid": True, "user": decoded.get("user")}
+        except (InvalidTokenError, ExpiredTokenError):
+            return {"valid": False}
+
     def __call__(self, parent, info, token: str):
         """Resolves the verify token mutation"""
-        token_verification = {}
-
-        try:
-            decoded = load_backend().decode(token)
-            token_verification["valid"] = True
-            token_verification["user"] = decoded.get("user")
-
-        except (InvalidTokenError, ExpiredTokenError):
-            token_verification["valid"] = False
-
-        return token_verification
+        self.token = token
+        return self.get_payload()
 
 
 resolve_verify_token = VerifyTokenResolver()
